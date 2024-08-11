452d5d7d3746962c8defe2c28c00ae53a96cfb98
abd-Raffay <rraffay38@gmail.com>
2024-08-11 13:37:54 +0500

    fixed status issue

--- api/server.js
+++ api/server.js
@@ -59,7 +59,13 @@ module.exports = async (req, res) => {
     };
 
     try {
-      await contactEmail.sendMail(mail);
+      contactEmail.sendMail(mail, (error) => {
+        if (error) {
+          res.json(error);
+        } else {
+          res.json({ code: 200, status: "Message Sent" });
+        }
+      });
       res.setHeader('Access-Control-Allow-Origin', 'https://personal-portfolio-nine-drab.vercel.app');
       res.status(200).json({ status: "Message Sent" });
     } catch (error) {
